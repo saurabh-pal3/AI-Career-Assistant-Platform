74f755d7ae2ccf5ff2d71150c2dbd69ea5688c3d
Your Name <your_email@example.com>
2026-03-21 20:06:42 +0530

    commit

--- src/main/java/com/aicareer/resume/ResumeServiceApplication.java
+++ src/main/java/com/aicareer/resume/ResumeServiceApplication.java
@@ -3,11 +3,14 @@ package com.aicareer.resume;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ResumeServiceApplication {
+public class ResumeServiceApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ResumeServiceApplication.class, args);
 	}
 
